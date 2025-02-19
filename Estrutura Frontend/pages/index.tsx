7e7d5fe889836cb68c639e429fbdbd2a00600451
karengoncalves8 <karengoncalves128@gmail.com>
2025-02-19 11:28:19 -0300

    fe-feat: added login components to index page

--- Estrutura Frontend/pages/index.tsx
+++ Estrutura Frontend/pages/index.tsx
@@ -1,16 +1,98 @@
+"use client"
 import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router';
+
+ 
+import { z } from "zod"
+ 
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(2).max(50)
+})
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
 
 export default function Home() {
 
     const router = useRouter();
 
+    const [username, setUsername] = useState()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          username: "",
+          password: "",
+        },
+    })
+     
+      // 2. Define a submit handler.
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+    
+
     return(
         <div className={styles.container}>
-            <h1>Home page</h1>
-            <button onClick={() => router.push('/about')}>
-                Go to about
-            </button>
+            <Card className={styles.card}>
+                <CardHeader>
+                    <CardTitle>Sign in</CardTitle>
+                    <CardDescription>Fill in the fields.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
+                                        <FormControl>
+                                            <Input  {...field} />
+                                        </FormControl>
+                                        <FormDescription>This is your public display name.</FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input type={'password'} {...field} />
+                                        </FormControl>
+                                        <FormDescription>This is your public display name.</FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
+                </CardContent>
+                <CardFooter>
+                    <Button onClick={() => router.push('/about')}>Log in</Button>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
